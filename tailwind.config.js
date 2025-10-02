ce40f6bc78a250c2f205a1d78a98f3710a84eb1b
AlexKGitahi <alexkgitahi@gmail.com>
2025-10-02 22:10:15 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}","./components/**/*.{js,jsx}"],
+  theme: { extend: { colors: { primary:'#2E7D32', secondary:'#25D366' } } },
+  plugins: [],
+}
